a69d0c682c92c6611956e615d3566b80397a5384
khalid umar <dreamiwoarld.online@gmail.com>
2022-03-23 16:04:46 +0600

    added new cart components

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
@@ -25,10 +26,7 @@ const Shop = () => {
           ></Product>
         ))}
       </div>
-      <div className="cart-container">
-        <h3>Order Summary</h3>
-        <p>Selected Items: {cart.length}</p>
-      </div>
+      <Cart cart={cart}></Cart>
     </div>
   );
 };
