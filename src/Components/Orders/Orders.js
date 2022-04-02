c2e86a8f302fd44f2ad3c6f411d0c50c9ad9d25c
khalid umar <dreamiwoarld.online@gmail.com>
2022-04-03 05:52:31 +0600

    comlete second custom hook

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import useCart from '../../Hooks/useCart';
+import useProducts from '../../Hooks/useProducts';
 
 const Orders = () => {
+  const [products, setProducts] = useProducts();
+  const [cart, setCart] = useCart(products);
   return (
     <div>
-      <h1>order page</h1>
+      <h1>this is orders: {products.length}</h1>
+      <p>cart has: {cart.length}</p>
     </div>
   );
 };
