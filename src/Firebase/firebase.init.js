299500e85a1f05a16268e8ef6c754c6dc52131fa
khalid umar <dreamiwoarld.online@gmail.com>
2022-04-10 03:24:20 +0600

    initialize firebase

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+  apiKey: 'AIzaSyCu-GLR9bE3Xp9mX8fRrRUcdq0hTZ2p43A',
+  authDomain: 'ema-jhon-ad0f8.firebaseapp.com',
+  projectId: 'ema-jhon-ad0f8',
+  storageBucket: 'ema-jhon-ad0f8.appspot.com',
+  messagingSenderId: '935615310582',
+  appId: '1:935615310582:web:b45807309f39f5d30fdad7',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
